3bc05a0c5d2ea52c588b785d8f314ed9cf3373f9
nandhp <nandhp@gmail.com>
2013-08-31 10:50:46 -0500

    Implement caching in WSGI implementation

--- wsgi.py
+++ wsgi.py
@@ -39,52 +39,75 @@ def timeout(seconds=10):
         return wraps(func)(wrapper)
     return decorator
 
-# Timeout searches after seven minutes. This allows excessively slow
+# Timeout searches after several minutes. This allows excessively slow
 # searches to be rejected entirely.
 @timeout(7*60)
 def run_search(query, year):
-    return iface.search(query, year=year)
+    results = iface.search(query, year=year)
+    return results[0] if results else None
+
+def format_response(query, year):
+    try:
+        result = run_search(query, year)
+    except TimeoutError:
+        # Return no results. The query is too complicated to
+        # complete in a reasonable amount of time. It probably
+        # would not produce any result, even with additional time.
+        # Retrying the request will likely produce in the same error.
+        return {'_error': 'Timeout with no results'}
+    if not result:
+        return {'_error': 'No results'}
+    obj = {'_score': result[1]}
+    for key in SUPPORTED_ARGS:
+        out = getattr(result[0], key)
+        # Convert names to "First Last"
+        if key in ('cast', 'directors', 'writers'):
+            for i, name in enumerate(out):
+                parsed_name = parse_name(name[0])
+                if parsed_name[1]:
+                    out[i] = (u' '.join(parsed_name[1:3]),) + name[1:]
+                else:
+                    out[i] = (parsed_name[2],)+name[1:]
+        obj[key] = out
+    return obj
+
+# Cache the last 50 search results for speed and to support retrys
+# after gateway timeout.
+CACHESIZE = 50
+searchcache = {}
+searchcache_mru = []
+
+def expire_cache():
+    # If the cache is full, expire a few old entries
+    if len(searchcache_mru) > CACHESIZE:
+        while len(searchcache_mru) > CACHESIZE-5:
+            item = searchcache_mru.pop(0)
+            del searchcache[item]
 
 def application(environ, start_response):
     path = environ.get('PATH_INFO', '')
     params = parse_qs(environ.get('QUERY_STRING',''))
     if path == '/imdb':
         ctype = 'application/json'
-        obj = {}
+        try:
+            year = int(params['y'][0])
+        except:
+            year = None
         if 'q' not in params or not params['q'] or not params['q'][0]:
-            obj['_error'] = 'No query provided'
+            obj = {'_error': 'No query provided'}
         else:
-            try:
-                year = int(params['y'][0])
-            except:
-                year = None
-            try:
-                results = run_search(params['q'][0], year)
-            except TimeoutError:
-                results = None
-                # Return no results. The query is too complicated to
-                # complete in a reasonable amount of time. It probably
-                # would not produce any result, even with additional time.
-                # Retrying the request will result in the same error.
-                obj['_error'] = 'Timeout with no results'
-            if '_error' in obj:
-                pass
-            elif not results:
-                obj['_error'] = 'No results'
+            query = params['q'][0]
+            # Look up query in the cache
+            cachekey = (query.lower(), year)
+            if cachekey in searchcache:
+                searchcache_mru.remove(cachekey)
             else:
-                obj['_score'] = results[0][1]
-                for key in SUPPORTED_ARGS:
-                    out = getattr(results[0][0], key)
-                    # Convert names to "First Last"
-                    if key in ('cast', 'directors', 'writers'):
-                        for i, name in enumerate(out):
-                            parsed_name = parse_name(name[0])
-                            if parsed_name[1]:
-                                out[i] = (u' '.join(parsed_name[1:3]),) + \
-                                    name[1:]
-                            else:
-                                out[i] = (parsed_name[2],)+name[1:]
-                    obj[key] = out
+                searchcache[cachekey] = format_response(query, year)
+                expire_cache()
+            # Fetch the item from the cache and update its MRU list position
+            obj = searchcache[cachekey]
+            searchcache_mru.append(cachekey)
+        # JSON-format the response
         response_body = json.dumps(obj)
     else:
         ctype = 'text/html'
