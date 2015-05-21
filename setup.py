0a6020bf65f25b3eea1d413f84f117bf831ef503
nandhp <nandhp@gmail.com>
2015-05-21 14:34:42 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='python-imdb',
-      version='0.1.16',
+      version='0.1.17',
       description='Python interface to IMDb plain-text data files',
       author='nandhp',
       author_email='nandhp@gmail.com',
